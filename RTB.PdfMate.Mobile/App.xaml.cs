fe3b346fce44704ab37adf61399c3361f8b4bcfc
Fabian Becker <becker@hanseaticsoft.com>
2025-04-05 08:45:57 +0200

    Refactor App.xaml.cs to set MainPage in CreateWindow method and update iOS platform version to 15.0 in project file

--- RTB.PdfMate.Mobile/App.xaml.cs
+++ RTB.PdfMate.Mobile/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace RTB.PdfMate.Mobile;
+﻿
+namespace RTB.PdfMate.Mobile;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+		return new Window(new AppShell());
+    }
 }
