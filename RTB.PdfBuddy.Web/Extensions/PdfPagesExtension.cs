296c49528348564c3813af485af1926bb07d98d7
IIFabixn <f.becker33@outlook.de>
2025-04-19 16:12:11 +0200

    Add PdfPagesExtension and enhance PDF editing features
    - Introduced `PdfPagesExtension` for page index retrieval.
    - Updated logo in `MainLayout.razor` for "PdfBuddy".
    - Restructured `Editor.razor` with new drop zones and improved file management.
    - Changed file selection handler in `Index.razor` to `ImportFiles`.
    - Added new SVG logo for PdfBuddy.
    - Enhanced `DropZone.razor` with `DraggingClass` for better UX.
    - Updated `_Imports.razor` with necessary namespaces.

---
+++ RTB.PdfBuddy.Web/Extensions/PdfPagesExtension.cs
@@ -0,0 +1,17 @@
+﻿using PdfSharpCore.Pdf;
+
+namespace RTB.PdfBuddy.Web.Extensions
+{
+    public static class PdfPagesExtension
+    {
+        public static int GetPageIndex(this PdfPages pages, PdfPage page)
+        {
+            for (int i = 0; i < pages.Count; i++)
+            {
+                if (pages[i] == page)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
